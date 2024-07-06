1645a8f5a86acd2d46d3683d505aa1758bf9e9a2
scyan00ffff <reubenjsykes@gmail.com>
2024-07-06 11:26:45 +0100

    Edited credentials files for puplic release

--- cred.py
+++ cred.py
@@ -1,3 +1,3 @@
-client_id='d4758fa8218e46258c45fb4e592a77d7'
-client_SECRET='7a1b893f8b6f4661ab4580463ea45fbd'   
-redirect_url='http://localhost:7777/callback'
+client_id='YOUR-CLIENT-ID'
+client_SECRET='YOUR-CLIENT-SECRET'   
+redirect_url='YOUR-REDIRECT-URL'
